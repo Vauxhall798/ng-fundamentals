3faa0fb5b1d644fe07eb09b7f1b5bad1894bc7f5
Vauxhall798 <vishaal351@gmail.com>
2022-02-10 17:58:59 -0800

    Changed

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -15,7 +15,7 @@ export class EventService{
   getEvent(id:number){
     return EVENT.find(event => event.id === id)
   }
-  private handleError<T>(operation = 'operation',result?:T){
+  private handleError<T>(_operation = 'operation',result?:T){
     return (error:any):Observable<T>=>{
       console.error(error);
       return of(result as T)
